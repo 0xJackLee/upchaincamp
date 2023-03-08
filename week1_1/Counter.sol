7706f2d3b7e944c8d46df7380826f4482f7b5db7
JackLee <280147597@qq.com>
2023-03-08 23:23:00 +0800

    upchaincamp exercise first commit

---
+++ week1_1/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.4;
+
+contract Counter {
+    uint256 public counter;
+    constructor () {
+        counter = 0;
+    }
+
+    function count() public {
+        counter = counter + 1;
+    }
+
+    function add(uint256 x) public {
+        counter += x;
+    }
+
+}
