dd14d21ab65af9d167c6e204c0fcf298688c3780
JackLee <280147597@qq.com>
2023-05-30 18:36:55 +0800

    update week4_1

--- week4_1/src/components/ERC20.vue
+++ week4_1/src/components/ERC20.vue
@@ -8,6 +8,9 @@ import erc2612Abi from "../../deployments/abi/ERC2612.json";
 import bankAddr from "../../deployments/dev/Bank.json";
 import bankAbi from "../../deployments/abi/Bank.json";
 
+import ethBankAbi from "../../deployments/abi/EthBank.json";
+import ethBankAddr from "../../deployments/dev/EthBank.json";
+
 import { ref, reactive } from "vue";
 
 const state = reactive({
@@ -30,6 +33,7 @@ let provider;
 let signer;
 let erc20Token;
 let bank;
+let ethBank;
 
 async function connect() {
   await initProvider();
@@ -77,6 +81,8 @@ async function initContract() {
   erc20Token = new ethers.Contract(erc2612Addr.address, erc2612Abi, signer);
 
   bank = new ethers.Contract(bankAddr.address, bankAbi, signer);
+  ethBank = new ethers.Contract(ethBankAddr.address, ethBankAbi, signer);
+  console.log("ethBank", ethBank);
 }
 
 async function readContract() {
@@ -109,13 +115,21 @@ async function readContract() {
 function transfer() {
   // 将一个值变成不带小数点的类型 实际数值为 amount*e18
   let amount = ethers.parseUnits(state.amount, 18);
-  signer.sendTransaction({
-    to: state.recipient,
-    value: 100,
-  });
+  //   signer.sendTransaction({
+  //     to: state.recipient,
+  //     value: 100,
+  //   });
   erc20Token.transfer(state.recipient, amount).then(async (r) => {
     console.log(r); // 返回值不是true
     await readContract();
+  });
+}
+
+function deposit() {
+  console.log(state.ethAmount);
+  let amount = ethers.parseEther(state.ethAmount);
+  ethBank.deposit({ value: amount }).then((r) => {
+    console.log(r);
   });
 }
 
@@ -130,55 +144,68 @@ async function transferEth() {
   console.log(ethAmount);
 }
 
+// ERC2612 存款
 async function permitDeposit() {
-  let nonce = await this.erc20Token.nonces(this.account);
-  this.deadline = Math.ceil(Date.now() / 1000) + parseInt(20 * 60);
+  // let nonce = await erc20Token.nonces(state.account);
+  // console.log(nonce)
+  let nonce = 0;
+  // 当前时间加上20分钟后的时间戳   Math.ceil将结果返回四舍五入最近的整数
+  let deadline = Math.ceil(Date.now() / 1000) + parseInt(20 * 60);
 
-  let amount = ethers.utils.parseUnits(this.stakeAmount).toString();
+  let amount = ethers.parseUnits(state.stakeAmount).toString();
 
+  //  signTypedData限定的格式
+  // 这些信息会在用户签名的时候显示
   const domain = {
     name: "ERC2612",
     version: "1",
-    chainId: this.chainId,
+    chainId: state.chainId,
     verifyingContract: erc2612Addr.address,
   };
 
+  // 消息格式 与ERC2612合约中的PERMIT_TYPEHASH类型保持一致
   const types = {
     Permit: [
-      { name: "owner", type: "address" },
-      { name: "spender", type: "address" },
-      { name: "value", type: "uint256" },
-      { name: "nonce", type: "uint256" },
-      { name: "deadline", type: "uint256" },
+      { name: "owner", type: "address" }, // 持有者
+      { name: "spender", type: "address" }, // 要允许花费的人
+      { name: "value", type: "uint256" }, // 要允许发送的数量
+      { name: "nonce", type: "uint256" }, // 随机数
+      { name: "deadline", type: "uint256" }, // 签名到期时间
     ],
   };
 
+  // 消息内容的具体值
   const message = {
-    owner: this.account,
+    owner: state.account,
     spender: bankAddr.address,
     value: amount,
     nonce: nonce,
-    deadline: this.deadline,
+    deadline: deadline,
   };
 
-  const signature = await this.signer._signTypedData(domain, types, message);
+  const signature = await signer.signTypedData(domain, types, message);
+  // console.log(splitSig);
   console.log(signature);
 
-  const { v, r, s } = ethers.utils.splitSignature(signature);
-
+  // const { v, r, s } = ethers.splitSignature(signature);
+  // const splitSig = ethers.Signature.from(signature);
+  // console.log(splitSig);
+  // console.log(splitSig.v);
+  // console.log(splitSig.r);
+  // console.log(splitSig.s);
+  const { v, r, s } = ethers.Signature.from(signature);
+  // let sigBytes = ethers.Signature.from(splitSig).serialized;
+  // console.log(sigBytes);
+  console.log(v);
+  console.log(r);
+  console.log(s);
   try {
-    let tx = await this.bank.permitDeposit(
-      this.account,
-      amount,
-      this.deadline,
-      v,
-      r,
-      s
-    );
+    let tx = await bank.permitDeposit(state.account, amount, deadline, v, r, s);
 
     let receipt = await tx.wait();
-    this.readContract();
+    readContract();
   } catch (e) {
+    console.log(e);
     alert("Error , please check the console log:", e);
   }
 }
@@ -228,6 +255,16 @@ async function permitDeposit() {
     <div>
       <input v-model="state.stakeAmount" placeholder="输入质押量" />
       <button @click="permitDeposit">离线授权存款</button>
+    </div>
+
+    <div
+      style="border-bottom: 1px solid black; width: 100%; margin: 10px 0"
+    ></div>
+    <div>
+      存款eth: 存款eth数量
+      <input type="text" v-model="state.ethAmount" />
+      <br />
+      <button @click="deposit">确定存款</button>
     </div>
   </div>
 </template>
